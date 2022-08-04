1eb1bae7add47a6f9cc53c8cfe9985136f12c8dc
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-08-04 21:31:26 +0100

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Top from "./pages/Top";
+import Create from "./pages/Create";
+import Flat from "./pages/Flat";
+import Edit from "./pages/Edit";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/flat:id" element={<Flat id="222" />} />
+        <Route path="/flat:id/edit" element={<Edit />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Flat.js
+++ src/pages/Flat.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const FLat = () => {
-  return <div>FLat</div>;
+const FLat = (id) => {
+  return (
+    <>
+      <div>FLat page</div>
+      {console.log(id)}
+    </>
+  );
 };
 
 export default FLat;
