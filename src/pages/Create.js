59ec6d1d8ff0d3ee73f203d76b89ea2822b7adff
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-08-05 23:52:03 +0100

    WIP: working on add create function

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,60 @@
-import React from "react";
+import { toBeInTheDocument } from "@testing-library/jest-dom/dist/matchers";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Create = () => {
-  return <div>Create page</div>;
+  const [lists, setLists] = useState([]);
+  const [address, setAddress] = useState("aiueo");
+  const [type, setType] = useState("flat");
+
+  const handleAddAddress = (e) => {
+    setAddress(e.target.value);
+  };
+
+  const handleChangeType = (e) => {
+    setType(e.target.value);
+  };
+
+  const handleClickCreate = () => {
+    const newLists = [
+      {
+        address: address,
+        type: type,
+        id: Math.floor(Math.random() * 100),
+      },
+      ...lists,
+    ];
+
+    console.log("newLists=", newLists);
+    setLists(newLists);
+
+    //画面遷移処理
+  };
+
+  console.log(lists);
+  return (
+    <>
+      <h2>Address</h2>
+      <input type="text" value={address} onChange={handleAddAddress} />
+      <h2>Type</h2>
+      <select onChange={handleChangeType}>
+        <option value="flat">Flat</option>
+        <option value="detached">Detached</option>
+        <option value="semi-detached">Semi-detached</option>
+        <option value="Terraced">Terraced</option>
+      </select>
+
+      <Link to="/" state={{ lists: lists }}>
+        <button
+          onClick={() => {
+            handleClickCreate();
+          }}
+        >
+          Create
+        </button>
+      </Link>
+    </>
+  );
 };
 
 export default Create;
--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Top = () => {
-  return <div>Top</div>;
+  const location = useLocation();
+  const [flatLists, setFlatLists] = useState([]);
+  console.log("location=", location.state);
+
+  return (
+    <>
+      <Link to="/create">
+        <button>Add</button>
+      </Link>
+      <ul>
+        {/* {lists.map((list) => {
+          return (
+            <li key={list.id}>
+              {list.id},{list.address},{list.type}
+            </li>
+          );
+        })} */}
+      </ul>
+    </>
+  );
 };
 
 export default Top;
